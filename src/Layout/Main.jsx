220612fc30fbcaf3a9c19eaae77002396ddb0ad2
MH01-Hasan <mehediph2021@gmail.com>
2023-10-09 23:03:04 +0600

    footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-// import Footer from "../pages/Shared/Footer/Footer";
-// import NavBar from "../pages/Shared/NavBar/NavBar";
+import Nabvar from "../pages/Shared/Nabvar/Nabvar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 
 const Main = () => {
     return (
         <div>
-            {/* <NavBar></NavBar> */}
+           <Nabvar></Nabvar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+           <Footer></Footer>
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,8 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>hello</h1>  
-            <p>amra 4 jon</p>   
+              
         </div>
     );
 };
